40dadf15f06254311efe323275284fbde4825dfb
goldengrape <github@goldengrape.org>
2023-08-04 11:56:19 -0700

    debug输出

--- backend.py
+++ backend.py
@@ -15,20 +15,22 @@ from data_process import (
     )
 import pandas as pd
 
-def print_tree(df, node_id=None, prefix="", is_last=False):
+def generate_tree_string(df, node_id=None, prefix="", is_last=False):
+    result = ""
     if node_id is None:
         node = df[df['parent_id'].isnull()].iloc[0]
     else:
         node = df[df['id'] == node_id].iloc[0]
 
-    # if node['name'] != 'Root':
     prefix_component = "└─ " if is_last else "├─ "
-    print(prefix + prefix_component + node['name'])
+    result += prefix + prefix_component + node['name'] + "\n"
 
     children = df[df['parent_id'] == node['id']]
     prefix_for_children = prefix + ("   " if is_last else "│  ")
     for i, (_, child) in enumerate(children.iterrows()):
-        print_tree(df, child['id'], prefix_for_children, i == len(children) - 1)
+        result += generate_tree_string(df, child['id'], prefix_for_children, i == len(children) - 1)
+    return result
+
 
 def add_item_from_human_input(df, human_input,debug=False):
     known_containers = get_containers_as_text(df)
@@ -41,24 +43,38 @@ def add_item_from_human_input(df, human_input,debug=False):
     new_df=add_data(df, result)
     return new_df 
 
-def query_item_from_human_query(df, human_query):
+def query_item_from_human_query(df, human_query,debug=False):
     better_human_query=better_query(human_query)
+    if debug:
+        print(better_human_query)
     ref_path=",".join(path_query(better_human_query, df))
-    answer=query_item(human_query, ref_path=ref_path)
+    if debug:
+        print(ref_path)
+    answer=query_item(human_query, ref_path=ref_path)    
     return answer
 
-def move_container_by_human_command(df, human_cmd):
+def move_container_by_human_command(df, human_cmd,debug=False):
     moving_names=structured_container_mover(human_cmd)
+    if debug:
+        print(moving_names)
     if moving_names['new_parent_name'] not in df['name'].values:
+        if debug:
+            print('new parent not in database')
         df=add_item_from_human_input(df, moving_names['new_parent_name'])
     df=move_container(df, 
         moving_names["container_name"], 
         moving_names['new_parent_name'])
+    if debug:
+        print(generate_tree_string(df))
     return df
 
-def takeout_items_by_human_command(df, human_cmd):
+def takeout_items_by_human_command(df, human_cmd,debug=False):
     takeout_items=structured_takeout_items(human_cmd)
+    if debug:
+        print(takeout_items)
     df=takeout_item_list(df,takeout_items)
+    if debug:
+        print(generate_tree_string(df))
     return df
 
 def run_human_command(df, human_cmd, debug=False):
@@ -79,5 +95,5 @@ def run_human_command(df, human_cmd, debug=False):
         else:
             df=eval(name)(df, human_input)
             if debug:
-                print_tree(df)
+                print(generate_tree_string(df))
     return df, answer_list
